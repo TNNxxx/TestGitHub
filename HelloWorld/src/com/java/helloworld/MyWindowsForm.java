23ee6f438b310d2d41361d7a3182e7803a71d3e2
TNNxxx <tnnfj@outlook.com>
2015-07-24 14:35:06 +0700

    เพิ่มโปรเจ็คเข้าไปใน 1 Repository

---
+++ HelloWorld/src/com/java/helloworld/MyWindowsForm.java
@@ -0,0 +1,43 @@
+package com.java.helloworld;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class MyWindowsForm {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MyWindowsForm window = new MyWindowsForm();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public MyWindowsForm() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
